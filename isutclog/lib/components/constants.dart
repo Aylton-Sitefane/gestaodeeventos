d2c6cf4e73af28f7c976eb87961f698bca9eac49
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2025-03-16 14:24:44 +0200

    Adicao da arquitectura e splash screen

---
+++ isutclog/lib/components/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//images
+const logoImage = AssetImage("assets/images/logo_isu.png");
+
+//colors
+
+// text_to_translate
+
+const welcomeText = 'O teu Futuro Começa Agora';
---
+++ isutclog/lib/splash_intro.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:isutclog/app/view/home/home_screen.dart';
+import 'package:isutclog/components/constants.dart';
+
+class SplashIntro extends StatefulWidget {
+  const SplashIntro({super.key});
+
+  @override
+  State<SplashIntro> createState() => _SplashIntroState();
+}
+
+class _SplashIntroState extends State<SplashIntro>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              Color(212529),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: logoImage),
+            SizedBox(height: 5,),
+            Text(welcomeText, style: TextStyle(fontSize: 22),)
+          ],
+        ),
+      ),
+    );
+  }
+}
